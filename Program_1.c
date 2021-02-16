2459c9129661c5f1d39c02136f30f557e915c18f
Shwetha15447 <shwethasgowdashwethasgowda@gmail.com>
2021-02-16 11:11:26 +0000

    assumed a value for x

--- Program_1.c
+++ Program_1.c
@@ -4,11 +4,11 @@ the word "TYPE" */
 #include<stdio.h>
 int main()
 {
-TYPE num = 100;
-TYPE letter = 'A';
+int x= 2500;
+char y='a';
 /* Printing num and charecter value */
-printf("%d\n",num);
-printf("%c\n",letter);
+printf("%d\n",x);
+printf("%c\n",y);
 /* if you done creerct you will get bellow message */
 printf("Good Job...");
 return 0;
